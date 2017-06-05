24c9ca7670249502c8936c7963e16c7239146aff
Andruka <kuzminovandrei1997@mail.ru>
2017-06-06 00:02:31 +0300

    лучше молчать,чтобы не сглазить

--- program/Cats.cpp
+++ program/Cats.cpp
@@ -36,7 +36,6 @@ istream & operator >> (istream & stream, Cat & cat) {
     cat.gender=buf[0];
     getline (stream, buf);
     cat.age=atoi(buf.c_str());
-    cout<<cat<<endl;
     return stream;
 }
 int Program::start(){
@@ -53,8 +52,8 @@ int Program::start(){
 	fin>>temp;
         if(fin.eof())return 0;
 	cat=temp;
-	if(i%2)fout2<<cat;
-	else fout1<<cat;
+	if(i%2)fout1<<cat;
+	else fout2<<cat;
 	}
     fin.close();
 }
@@ -62,253 +61,155 @@ int Program::compare(Cat cat1,Cat cat2,int type){
 if(type==1){
     string buf1=cat1.name,buf2=cat2.name;
     for(int i=0;;++i){
-        if(buf1[i]<buf2[i])return 1;
+        if(buf1[i]<buf2[i])return 0;
 	else{
-	    if(buf1[i]!=buf2[i])return 2;
+	    if(buf1[i]!=buf2[i])return 1;
 	    }
-	if(buf1.size()==i+1)return 1;
-	if(buf2.size()==i+1)return 2;
+	if(buf1.size()==i+1)return 0;
+	if(buf2.size()==i+1)return 1;
         }
     }
 if(type==2){
     string buf1=cat1.breed,buf2=cat2.breed;
     for(int i=0;;++i){
-        if(buf1[i]<buf2[i])return 1;
+        if(buf1[i]<buf2[i])return 0;
 	else{
-	    if(buf1[i]!=buf2[i])return 2;
+	    if(buf1[i]!=buf2[i])return 1;
 	    }
-	if(buf1.size()==i+1)return 1;
-	if(buf2.size()==i+1)return 2;
+	if(buf1.size()==i+1)return 0;
+	if(buf2.size()==i+1)return 1;
         }
     }
 if(type==3){
-    if(cat1.age<=cat2.age)return 1;
-    else return 2;
+    if(cat1.age<=cat2.age)return 0;
+    else return 1;
     }
 }
 void Program::sort(int type){
-    const char * F1="1.txt";
-    const char * F2="2.txt";
-    const char * F3="3.txt";
-    const char * F4="4.txt";
+    int block[2];
     int x=1;
-    int blockFin1,blockFin2;
-    int checkFin;
-    bool checkFout;
-    int checkCat;
-    bool checkFinish1,checkFinish2;
-    Cat cat1;
-    Cat cat2;
-    Cat temp;
+    int a;
+    int checkCat,actualFin;
+    Cat cat[2];
+    ifstream fin[2];
+    ofstream fout[2];
     while(true){
-	blockFin1=x-1,blockFin2=x;
-	checkFin=2;
-	checkFinish1=checkFinish2=checkFout=false;
-	checkCat=2;
-	ifstream fin1(F1),fin2(F2);
-	ofstream fout1(F3),fout2(F4);
-	fin1>>temp;
-        if(fin1.eof()){rename("2.txt","result.txt");return;}
-	cat1=temp;
-	    while(blockFin1 || blockFin2){
-		if(blockFin1==x && blockFin2==x){
-		    if(checkCat==1){
-			if(checkFout)fout2<<cat2;
-			else fout1<<cat2;
-			}
-		    else{
-			if(checkFout)fout2<<cat1;
-			else fout1<<cat1;
-			}
-		    if(checkFin==1){
-			fin2>>temp;
-			if(fin2.eof()){
-			if(blockFin2!=x)checkFinish2=true;
-			if(!fin1.eof())blockFin1+=blockFin2;
-			blockFin2=0;continue;
-			}
-			if(checkCat==1)cat2=temp;
-			else cat1=temp;
-			--blockFin2;
-			}
-		    else{
-			fin1>>temp;
-			if(fin1.eof()){
-			if(blockFin1!=x)checkFinish1=true;
-			if(!fin2.eof())blockFin2+=blockFin1;
-			blockFin1=0;continue;
-			}
-			if(checkCat==1)cat2=temp;
-			else cat1=temp;
-			--blockFin1;			
-			}
-		    }
-		else{
-		    if(blockFin1==x && fin2.eof()){
-			fin1>>temp;
-			if(fin1.eof()){
-			    blockFin1=0;continue;
-			    }
-		        if(checkCat==1){
-			    if(checkFout)fout2<<cat2;
-			    else fout1<<cat2;
-			    }
-		        else{
-			    if(checkFout)fout2<<cat1;
-			    else fout1<<cat1;
-			    }
-			if(checkCat==1)cat2=temp;
-			else cat1=temp;
-			--blockFin1;		    		    
-		        }
-		    else{
-			if(blockFin2==x && fin1.eof()){
-			    fin2>>temp;
-			    if(fin2.eof()){blockFin2=0;continue;}
-		            if(checkCat==1){
-			        if(checkFout)fout2<<cat2;
-			        else fout1<<cat2;
-			        }
-		            else{
-			        if(checkFout)fout2<<cat1;
-			        else fout1<<cat1;
-			        }
-			    if(checkCat==1)cat2=temp;
-			    else cat1=temp;
-			    --blockFin2;					    
-		            }
-		        }
-  		    }
-		if(!blockFin1 || !blockFin2){
-		    if(blockFin1==0){
-			while(blockFin2!=0){
-			    fin2>>temp;
-			    if(fin2.eof()){
-				if(blockFin2!=x)checkFinish2=true;
-				if(!fin1.eof())blockFin1+=blockFin2;
-				blockFin2=0;				
-				break;
+	block[0]=block[1]=x;
+	fin[0].open("1.txt");
+	fin[1].open("2.txt");
+	fout[0].open("3.txt");
+	fout[1].open("4.txt");
+   	for(int i=1;;++i){
+	    a=(i%2);
+	    if(fin[0].eof() && fin[1].eof())break;
+	    for(int j=(x*2);j>0;--j){
+		if(!fin[0].eof() || !fin[1].eof()){
+	            if(j==(x*2) && !fin[0].eof()){
+		        fin[0]>>cat[0];
+			if(fin[0].eof()){
+			    if(i==1 && j==x*2){
+	    			rename("2.txt","result.txt");
+	    			return;
 				}
-			    --blockFin2;
-			    if(checkCat==1)cat1=temp;
-			    else cat2=temp;
-			    if(compare(cat1,cat2,type)==1){
-			    if(checkFout)fout1<<cat1;
-				else fout2<<cat1;
-				checkCat=1;
-			 	}
-			    else{
-				if(checkFout)fout1<<cat2;
-				else fout2<<cat2;
-				checkCat=2;				    
-				}
+			    ++j;
+			    block[1]+=block[0];
+			    block[0]=0;
+			    continue;
 			    }
-			if(blockFin1==0 || blockFin2==0)blockFin1=blockFin2=x;
-		 	if(fin1.eof() ||fin2.eof()){
-			    if(fin1.eof())blockFin1=0;
-			    if(fin2.eof())blockFin2=0;
-			    }
-			if(!fin1.eof() || !fin2.eof()){
-			    if(checkFout)checkFout=false;
-			    else checkFout=true;
-			    }
+			--block[0];
 			continue;
 			}
-		    else{
-			while(blockFin1!=0){
-			    fin1>>temp;
-			    if(fin1.eof()){
-				if(blockFin1!=x)checkFinish1=true;
-				if(!fin2.eof())blockFin2+=blockFin1;
-				blockFin1=0;
-				break;
-				}
-			    --blockFin1;
-			    if(checkCat==1)cat1=temp;
-			    else cat2=temp;
-			    if(compare(cat1,cat2,type)==1){
-				if(checkFout)fout1<<cat1;
-				else fout2<<cat1;
-				checkCat=1;
+		    if(j==(x*2)){
+			fin[1]>>cat[0];
+			if(fin[1].eof()){
+			    ++j;
+			    block[0]+=block[1];
+			    block[1]=0;
+			    continue;
+			    }
+			--block[1];
+			continue;
+			}
+		    if(j==(x*2-1)){
+			fin[1]>>cat[1];
+			if(fin[1].eof()){
+			    ++j;
+			    block[0]+=block[1];
+			    block[1]=0;
+			    fout[a]<<cat[0];			    
+			    continue;
+			    }
+			--block[1];
+		        checkCat=compare(cat[0],cat[1],type);
+		        fout[a]<<cat[checkCat];
+		        if(j==1){
+			    if(checkCat==0)fout[a]<<cat[1];
+			    else fout[a]<<cat[0];
+			    }
+			continue;						
+			}
+		    if(block[0]!=0 && block[1]!=0){
+		    	fin[checkCat]>>cat[checkCat];
+			if(fin[checkCat].eof()){
+			    if(fin[0].eof()){
+			        block[1]+=block[0];
+			        block[0]=0;				
 				}
 			    else{
-				if(checkFout)fout1<<cat2;
-				else fout2<<cat2;	
-				checkCat=2;			    
+			        block[0]+=block[1];
+			        block[1]=0;				
 				}
+			    ++j;
+			    continue;
 			    }
-			if(blockFin1==0 || blockFin2==0)blockFin1=blockFin2=x;
-		 	if(fin1.eof() ||fin2.eof()){
-			    if(fin1.eof())blockFin1=0;
-			    if(fin2.eof())blockFin2=0;
+			--block[checkCat];
+		    	checkCat=compare(cat[0],cat[1],type);
+		    	fout[a]<<cat[checkCat];
+			}
+		    else{
+			if(block[0]==0)actualFin=1;
+			else actualFin=0;
+		        fin[actualFin]>>cat[checkCat];
+			if(fin[actualFin].eof()){
+			    if(fin[0].eof()){
+			        block[1]+=block[0];
+			        block[0]=0;				
+				}
+			    else{
+			        block[0]+=block[1];
+			        block[1]=0;				
+				}
+			    if(fin[0].eof() && fin[1].eof()){
+				if(checkCat==0)fout[a]<<cat[1];
+				else fout[a]<<cat[0];				
+				}
+			    ++j;
+			    continue;
 			    }
-			if(!fin1.eof() || !fin2.eof()){
-			    if(checkFout)checkFout=false;
-			    else checkFout=true;
-			    }
-			continue;			    
+			--block[actualFin];
+		        checkCat=compare(cat[0],cat[1],type);
+		        fout[a]<<cat[checkCat];			
+			}
+		    if(j==1){
+			if(checkCat==0)fout[a]<<cat[1];
+			else fout[a]<<cat[0];
 			}
 		    }
-		if(checkFin==1){
-		    fin1>>temp;
-	 	    if(fin1.eof()){
-			if(blockFin1!=x)checkFinish1=true;
-			blockFin2+=blockFin1;
-			blockFin1=0;continue;
-			}
-		    --blockFin1;
-		    }
-		if(checkFin==2){
-		    fin2>>temp;
-	 	    if(fin2.eof()){
-			if(blockFin2!=x)checkFinish2=true;
-			blockFin1+=blockFin2;
-			blockFin2=0;continue;
-			}
-		    --blockFin2;
-		    }
-		if(checkCat==1)cat1=temp;
-		else cat2=temp;
-		if(compare(cat1,cat2,type)==1){
-		    if(checkFout)fout1<<cat1;
-		    else fout2<<cat1;
-		    if(checkCat==2){
-			if(checkFin==1)checkFin=2;
-			else checkFin=1;
-			}
-		    checkCat=1;	
-		    }
-		else{
-		    if(checkFout)fout1<<cat2;
-		    else fout2<<cat2;
-		    if(checkCat==1){
-			if(checkFin==1)checkFin=2;
-			else checkFin=1;
-			}
-		    checkCat=2;			
-		    }
-		}
-	if(checkFinish1 && checkFinish2){
-	    if(checkCat==1){
-		if(checkFout)fout1<<cat2;
-		else fout2<<cat2;
-		}
-	    else{
-		if(checkFout)fout1<<cat1;
-		else fout2<<cat1;		
-		}
+		else break;
+	    	}
+	    block[0]=block[1]=x;
 	    }
-	x=x*2;
-	fin1.close();
-	fin2.close();
-	fout1.close();
-	fout2.close();
+	fin[0].close();
+	fin[1].close();
+	fout[0].close();
+	fout[1].close();
 	rename("1.txt","temp.txt");
 	rename("3.txt","1.txt");
 	rename("temp.txt","3.txt");
 	rename("2.txt","temp.txt");
 	rename("4.txt","2.txt");
 	rename("temp.txt","4.txt");
+	x=x*2;		
 	}
 }
 void Program::work(){
